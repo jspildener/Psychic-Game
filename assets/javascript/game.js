f04d6a7c8206a3746d893982b6b090af7e1dd64d
William R Poulson <jessica.spildener@gmail.com>
2017-07-30 14:50:17 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+ var wins = 0;
+ var losses = 0;
+ var guessesLeft = 9;
+ var guessesSoFar = [];
+ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
